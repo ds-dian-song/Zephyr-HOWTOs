ea4e98cef066e5c0a7a55438159fac87f7dcf60e
Dian Song <ds@dian-song.com>
2022-09-28 11:57:06 -0700

    drop files from .gitignore

--- smp_svr/build/image_preload.cmake
+++
@@ -1,9 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "bl653_dvk" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/Users/SongDian/Desktop/smp_svr2/smp_svr;c:/Users/SongDian/Desktop/alpha4firmware/hello_world3;C:/Users/SongDian/ncs2/v2.0.2/nrf;C:/Users/SongDian/ncs2/v2.0.2/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "c:/Users/SongDian/Desktop/smp_svr2/smp_svr/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "c:/Users/SongDian/Desktop/smp_svr2/smp_svr/bl653_dvk.overlay" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "c:/Users/SongDian/Desktop/smp_svr2/smp_svr/bl653_dvk.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(OVERLAY_CONFIG "c:/Users/SongDian/Desktop/smp_svr2/smp_svr/overlay-bt.conf;c:/Users/SongDian/Desktop/smp_svr2/smp_svr/overlay-fs.conf;c:/Users/SongDian/Desktop/smp_svr2/smp_svr/overlay-cdc.conf;c:/Users/SongDian/Desktop/smp_svr2/smp_svr/overlay-serial.conf;c:/Users/SongDian/Desktop/smp_svr2/smp_svr/overlay-serial-console.conf;c:/Users/SongDian/Desktop/smp_svr2/smp_svr/overlay-shell-mgmt.conf;c:/Users/SongDian/Desktop/smp_svr2/smp_svr/overlay-shell.conf" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "c:\Users\SongDian\ncs2\toolchains\v2.0.2\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
